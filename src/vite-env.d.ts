bdc59a2aaf6f968b6bc360a4f40f078e23de37c2
kekoyana <kekoyana0@gmail.com>
2025-01-08 19:16:38 +0900

    githubpagesに合わせてパス修正

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/gomoku_ts/', // リポジトリ名に合わせてベースパスを設定
+})
